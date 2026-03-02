5153b16ed5d7653d81207d7d3e2517e95156a717
Milton Isaya <miltonisaiah@gmail.com>
2026-03-02 13:15:09 +0300

    set app display name to "UCS Coordinator's Reports"

--- d2.config.js
+++ d2.config.js
@@ -1,5 +1,7 @@
 const config = {
     type: 'app',
+    name: "UCS Coordinator's Reports",
+    title: "UCS Coordinator's Reports",
     icon: './logo.png',
     entryPoints: {
         app: './src/App.jsx',
